93a7049cdf5229ccfa15e533d45311f1b7da6353
Narandaev Damir <narandaevd@mail.ru>
2022-01-28 06:49:03 +0300

    Cors was added.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path =  require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT | 3000;
 const distDir = path.resolve(__dirname, 'dist');
@@ -8,6 +9,11 @@ const app = express();
 
 app.use(express.json({extended: true}));
 app.use(express.static(distDir));
+app.use(cors({
+    origin: '*',
+    methods: ['GET'],
+    credentials: true,
+}));
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(distDir, 'index.html'));
