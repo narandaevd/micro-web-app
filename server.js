50c9cb5dc8bce40857cfb7b33dd41051903cb4e2
Narandaev Damir <narandaevd@mail.ru>
2022-01-28 20:12:44 +0300

    Favicon was added.

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const path =  require('path');
 const cors = require('cors');
+const favicon = require('express-favicon');
 
 const PORT = process.env.PORT | 3000;
 const distDir = path.resolve(__dirname, 'dist');
 
 const app = express();
 
+app.use(favicon(path.resolve(distDir, 'img', 'favicon.png')));
 app.use(express.json({extended: true}));
 app.use(express.static(distDir));
-app.use(cors({
-    origin: '*',
-    methods: ['GET'],
-    credentials: true,
-}));
+app.use(cors({ origin: '*', methods: ['GET'], credentials: true }));
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.resolve(distDir, 'index.html'));
 })
 
